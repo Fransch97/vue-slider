feebc8e862f3755b7f6faff3ffebaa2473efb6ad
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-03 14:48:19 +0200

    starting vue slide

---
+++ js/script-sc.js
@@ -0,0 +1,19 @@
+const app = new Vue({
+    el: "#app",
+
+    data:{
+
+    },
+
+    methods: {
+        
+    },
+
+    
+
+    mounted() {
+        console.log("working")
+        console.log(slides)
+    }
+
+})
